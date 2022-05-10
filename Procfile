54ffd0ede95c7c7b03d5bb9ebb2481cd45aaf5a6
marko <markogojkovi045@gmail.com>
2022-05-10 11:45:39 +0100

    added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guniconr forum4.wsgi
--- forum4/settings.py
+++ forum4/settings.py
@@ -123,7 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
+
 
 
 STATICFILES_DIRS = [
