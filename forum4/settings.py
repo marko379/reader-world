ad7d837bbe7b45fd0aca935ec81379f2c40ed414
marko <markogojkovi045@gmail.com>
2022-05-24 11:24:15 +0100

    final setup 28 debug true

--- forum4/settings.py
+++ forum4/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('db_pass')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1' ,'reader-world.herokuapp.com']
 
