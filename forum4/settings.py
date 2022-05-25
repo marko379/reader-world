9b6e2f4e6f585676ea786a6a6608862f4312d4ee
marko <markogojkovi045@gmail.com>
2022-05-25 08:16:45 +0100

    final setup 31 debug true

--- forum4/settings.py
+++ forum4/settings.py
@@ -165,7 +165,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_HOST_USER = os.environ.get('HOST_EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('HOST_EMAIL_PASSWORD')
+# EMAIL_HOST_PASSWORD = os.environ.get('HOST_EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = 'esmeledxqgrdncav'
 EMAIL_PORT = 587
 
 AWS_ACCESS_KEY_ID =  os.environ.get('AWS_ACCESS_KEY_ID')
