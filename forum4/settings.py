4313f4681bc37f26bd7ebf047054d70d6ccdecf5
marko379 <56532877+marko379@users.noreply.github.com>
2023-01-22 16:40:33 +0000

    Update settings.py

--- forum4/settings.py
+++ forum4/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('db_pass')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' ,'reader-world.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1' ,'reader-world.herokuapp.com','https://web-production-3326.up.railway.app/']
 
 
 # Application definition
